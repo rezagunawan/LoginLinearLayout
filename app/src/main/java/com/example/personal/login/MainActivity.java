348c8bc625b032519e135da015785ca47f083e51
ulfah choirun nissa <ulfahcn@gmail.com>
2017-10-13 12:27:27 +0700

    Tugas Login Linear Layout Mopro

---
+++ app/src/main/java/com/example/personal/login/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.personal.login;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static com.example.personal.login.R.id.editText_main;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText editText = (EditText) findViewById(editText_main);
+
+    }
+
+
+
+
+
+
+
+}
+
